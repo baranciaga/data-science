931a339e4dcfe4a409ec4a2341d6a26b41a138ed
baranciaga <baranagit@hotmail.de>
2021-06-05 22:01:37 +0200

    Added further examples of streamlit usage

--- scripts/sl_visualize.py
+++ scripts/sl_visualize.py
@@ -1,11 +1,21 @@
 import streamlit as st
-
+import yfinance as yf
 st.write("""
 # My first app!
 Hello, world!
 """)
 number = st.slider("Pick a number", 0, 69,)
-date = st.date_input('Pick a Date!')
+st.write("""
+## PHAM ALTER!!! 
+""")
+
+ticker_symbol = 'BB'
+
+ticker_data = yf.Ticker(ticker_symbol)
+ticker_df = ticker_data.history(period='1y', start='2020-6-2', end='2021-6-3')
+st.line_chart(ticker_df.Open)
+st.line_chart(ticker_df.Close)
+st.line_chart(ticker_df.Volume)
 
 @st.cache
 def streamlit():
