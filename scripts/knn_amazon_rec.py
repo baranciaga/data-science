637c07cd10a0ba50bfe1e4169e6bdd78ffb0347f
duph97 <dungvn97@outlook.com>
2021-06-15 11:52:59 +0200

    auf kNNBaseline geswitcht und precision als evaluation geadded

--- scripts/knn_amazon_rec.py
+++ scripts/knn_amazon_rec.py
@@ -1,29 +1,52 @@
+from collections import defaultdict
 import pandas as pd
-from surprise import Dataset, KNNBasic, KNNBaseline, KNNWithZScore, SVD, accuracy
+from surprise import Dataset,KNNBaseline, accuracy
 from surprise import Reader
-from surprise import KNNWithMeans
 from surprise.model_selection import train_test_split
+# von https://surprise.readthedocs.io/en/stable/FAQ.html
+def get_top_n(predictions, n=10):
+    """Return the top-N recommendation for each user from a set of predictions.
+    Args:
+        predictions(list of Prediction objects): The list of predictions, as
+            returned by the test method of an algorithm.
+        n(int): The number of recommendation to output for each user. Default
+            is 10.
+    Returns:
+    A dict where keys are user (raw) ids and values are lists of tuples:
+        [(raw item id, rating estimation), ...] of size n.
+    """
 
+    # First map the predictions to each user.
+    top_n = defaultdict(list)
+    for uid, iid, true_r, est, _ in predictions:
+        top_n[uid].append((iid, est))
+
+    # Then sort the predictions for each user and retrieve the k highest ones.
+    for uid, user_ratings in top_n.items():
+        user_ratings.sort(key=lambda x: x[1], reverse=True)
+        top_n[uid] = user_ratings[:n]
+
+    return top_n
 # Daten in ein pandas dataframe einlesen, file liegt im Projektordner, header muss erstellt werden weil keine im original file sind
-df = pd.read_csv('../data/ratings_amazon.csv',header = None , usecols = [0,1,2], names=['userId','movieId', 'rating'], nrows = 100000)
+df = pd.read_csv('../data/ratings_amazon.csv',header = None , usecols = [0,1,2], names=['userId','productId', 'rating'], nrows = 100000)
 print(df)
 
 # pandas dataframe in ein Surprise data object umwandeln
 reader = Reader(rating_scale=(1, 5))
-data = Dataset.load_from_df(df[["userId", "movieId", "rating"]], reader)
+data = Dataset.load_from_df(df[["userId", "productId", "rating"]], reader)
 
 # test und trainset erstellen
 x_train, x_test= train_test_split(data, train_size=0.2, test_size= 0.8)
 print("train data \n", x_train)
 #print("test data \n", x_test)
 
-# To use item-based cosine similarity
+# To use item-based pearson similarity
 sim_options = {
-    "name": "pearson", #oder cosine, oder pearson_baseline
-    "user_based": True,  # Compute  similarities between items
+    "name": "pearson",
+    "user_based": False,  # Compute  similarities between items
 }
 # definition des algo objekts
-algo1 = KNNWithMeans(k=2, sim_options=sim_options)
+algo1 = KNNBaseline(k=2, sim_options=sim_options)
 
 # algo trainieren
 algo1.fit(x_train)
@@ -35,3 +58,10 @@ predictions1 = algo1.test(x_test)
 accuracy.rmse(predictions1)
 accuracy.mae(predictions1)
 accuracy.mse(predictions1)
+
+# für jeden user die 5 items, wo wir predicten dass er sie hoch bewertet, holen
+top_n = get_top_n(predictions1, n=5)
+
+# Print the recommended items for each user
+for uid, user_ratings in top_n.items():
+    print(uid, [iid for (iid, _) in user_ratings])
--- scripts/knn_movie_rec.py
+++ scripts/knn_movie_rec.py
@@ -1,9 +1,8 @@
 from collections import defaultdict
 
 import pandas as pd
-from surprise import Dataset, KNNBasic, KNNBaseline, KNNWithZScore, SVD, accuracy
+from surprise import Dataset,KNNBaseline, accuracy
 from surprise import Reader
-from surprise import KNNWithMeans
 from surprise.model_selection import train_test_split
 
 # von https://surprise.readthedocs.io/en/stable/FAQ.html
@@ -30,7 +29,42 @@ def get_top_n(predictions, n=10):
         top_n[uid] = user_ratings[:n]
 
     return top_n
+def precision_recall_at_k(predictions, k=10, threshold=3.5):
+    """Return precision and recall at k metrics for each user"""
 
+    # First map the predictions to each user.
+    user_est_true = defaultdict(list)
+    for uid, _, true_r, est, _ in predictions:
+        user_est_true[uid].append((est, true_r))
+
+    precisions = dict()
+    recalls = dict()
+    for uid, user_ratings in user_est_true.items():
+
+        # Sort user ratings by estimated value
+        user_ratings.sort(key=lambda x: x[0], reverse=True)
+
+        # Number of relevant items
+        n_rel = sum((true_r >= threshold) for (_, true_r) in user_ratings)
+
+        # Number of recommended items in top k
+        n_rec_k = sum((est >= threshold) for (est, _) in user_ratings[:k])
+
+        # Number of relevant and recommended items in top k
+        n_rel_and_rec_k = sum(((true_r >= threshold) and (est >= threshold))
+                              for (est, true_r) in user_ratings[:k])
+
+        # Precision@K: Proportion of recommended items that are relevant
+        # When n_rec_k is 0, Precision is undefined. We here set it to 0.
+
+        precisions[uid] = n_rel_and_rec_k / n_rec_k if n_rec_k != 0 else 0
+
+        # Recall@K: Proportion of relevant items that are recommended
+        # When n_rel is 0, Recall is undefined. We here set it to 0.
+
+        recalls[uid] = n_rel_and_rec_k / n_rel if n_rel != 0 else 0
+
+    return precisions, recalls
 # Daten in ein pandas dataframe einlesen, file liegt im Projektordner
 df = pd.read_csv("../data/ml-latest-small/ratings.csv", usecols = ['userId','movieId', 'rating'])
 print(df)
@@ -44,13 +78,13 @@ x_train, x_test= train_test_split(data, train_size=0.5, test_size= 0.5)
 print("train data \n", x_train)
 #print("test data \n", x_test)
 
-# To use item-based cosine similarity
+# To use item-based pearson similarity
 sim_options = {
-    "name": "pearson", #oder cosine, oder pearson_baseline
-    "user_based": True,  # Compute  similarities between items
+    "name": "pearson",
+    "user_based": False,  # Compute  similarities between items
 }
 # definition des algo objekts
-algo1 = KNNWithMeans(k=2, sim_options=sim_options)
+algo1 = KNNBaseline(k=2, sim_options=sim_options)
 
 # algo trainieren
 algo1.fit(x_train)
@@ -58,10 +92,7 @@ algo1.fit(x_train)
 # algo testen
 predictions1 = algo1.test(x_test)
 
-# Evaluations berechnen
-accuracy.rmse(predictions1)
-accuracy.mae(predictions1)
-accuracy.mse(predictions1)
+
 
 # für jeden user die 5 items, wo wir predicten dass er sie hoch bewertet, holen
 top_n = get_top_n(predictions1, n=5)
@@ -69,3 +100,13 @@ top_n = get_top_n(predictions1, n=5)
 # Print the recommended items for each user
 for uid, user_ratings in top_n.items():
     print(uid, [iid for (iid, _) in user_ratings])
+
+# Evaluations berechnen
+accuracy.rmse(predictions1)
+accuracy.mae(predictions1)
+accuracy.mse(predictions1)
+# Precision
+precisions, recalls = precision_recall_at_k(predictions1, k=5, threshold=3.5)
+# Precision and recall can then be averaged over all users
+print(sum(prec for prec in precisions.values()) / len(precisions))
+print(sum(rec for rec in recalls.values()) / len(recalls))
