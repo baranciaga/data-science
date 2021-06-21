1d24c3191afea42cf6b55e6985e86e9ff19b169b
baranciaga <baranagit@hotmail.de>
2021-06-22 00:13:54 +0200

    Zwischenstand pushen

--- scripts/sl_visualize.py
+++ scripts/sl_visualize.py
@@ -1,10 +1,10 @@
 import streamlit as st
-# from knn_movie_rec import apply_kNN_movie, get_top_n
+from knn_movie_rec import apply_kNN_movie, get_top_n
 import pandas as pd
 from utils import load_dataset, get_value_counts
-# from nnmf_movie_rec import run_neural_network
-
-
+# from nnmf_movie_rec import run_neural_network, recommend
+from tensorflow import keras
+import numpy as np
 
 
 
@@ -17,72 +17,79 @@ def main():
     st.write("""
     # Barans and Dungs Recommender
     """)
-
-    # Choice of the algo
-    page_options = ["K-nearest neighbor", "Neural network matrix factorization"]
-    page_selection = st.sidebar.selectbox("Choose Option", page_options)
-    if page_selection == "K-nearest neighbor":
-        choice_dataset = st.selectbox('Please select the dataset:', ('Amazon reviews', 'Movielens movie reviews'))
-
-        # choice of dataset, this code is duplicated in line 62-67
-        if choice_dataset == 'Amazon reviews':
-            path_to_dataset = "../data/ratings_amazon.csv"
-            cols = [0, 1, 2]
-
-        elif choice_dataset == 'Movielens movie reviews':
-            path_to_dataset = "../data/ml-latest-small/ratings.csv"
-            cols = ['userId', 'movieId', 'rating']
+    # sidebar options
+    choice_dataset = st.sidebar.selectbox('Please select the dataset:', ('Amazon reviews', 'Movielens movie reviews'))
+    threshold = st.sidebar.number_input('Threshold: ', value=100)
+    choice_of_algo = st.sidebar.selectbox("Which Similarity metric?", ("pearson", "cosine"))
 
 
+    if choice_dataset == 'Amazon reviews':
 
+        path_to_dataset = "../data/ratings_amazon.csv"
+        cols = ['userId', 'itemId', 'rating']
+
+        # load dataset
         df = load_dataset(path=path_to_dataset, cols=cols)
 
+        st.write("Head of dataframe: ")
+        st.write(df.head(20))
+
+        # Variance of the dataset as bar chart
+        st.write('Number of ratings:')
         s = get_value_counts(df)
         df = pd.DataFrame(s).T
         st.bar_chart(df)
+        df_amazon = pd.read_csv('../data/ratings_amazon.csv', header=None, usecols=[0, 1, 2],
+                                names=['userId', 'productId', 'rating'], nrows=50000)
+        df_amazon = df_amazon.pivot(index='userId', columns='productId', values='rating')
 
-        #output of data
-        st.write("Shape of Data", df.shape)
-        st.write(df.head(20))
-        # input variables for the knn algo:
-        # number = st.slider("Popularity Threshold", 1, 5, )
-        threshold = st.sidebar.number_input('Threshold: ', value=1)
-        choice_of_k = st.sidebar.number_input("Choose k", value=10)
-        choice_of_n = st.sidebar.number_input("How many recommendations do you wanna get?", value=10)
-        choice_of_algo = st.sidebar.selectbox("Which Similarity metric?", ("pearson", "cosine"))
+        st.write('pivoted data: ', df_amazon.head(20))
 
-        # button for knn algo
-        if st.sidebar.button('Run KNN Algo'):
-            # result, error = apply_kNN_movie(int(threshold), str(choice_of_algo), int(choice_of_k), int(choice_of_n))
-            # get_top_n(result)
-            st.write("Placeholder")
-
-    if page_selection == "Neural network matrix factorization":
-        choice_dataset = st.selectbox('Please select the dataset:', ('Amazon reviews', 'Movielens movie reviews'))
-
-        if choice_dataset == 'Amazon reviews':
-            path_to_dataset = "../data/ratings_amazon.csv"
-            cols = [0, 1, 2]
-        elif choice_dataset == 'Movielens movie reviews':
-            path_to_dataset = "../data/ml-latest-small/ratings.csv"
-            cols = ['userId', 'movieId', 'rating']
-
-        # load ds from utils.py
+    elif choice_dataset == 'Movielens movie reviews':
+        # load dataset
+        path_to_dataset = "../data/ml-latest-small/ratings.csv"
+        cols = ['userId', 'movieId', 'rating']
         df = load_dataset(path=path_to_dataset, cols=cols)
+
+        # barchart of unique ratings -> variance
+        st.write('Number of ratings:')
+        s = get_value_counts(df)
+        df = pd.DataFrame(s).T
+        st.bar_chart(df)
+        print("Ratings Df: ", df)
+
         st.write("Shape of Data", df.shape)
         st.write(df.head(20))
 
-        # parameters fro NNMF
-        n_epochs = st.sidebar.slider('Epochs', 1, 20)
-        choice_metrics = st.sidebar.selectbox('Which metrics?', ('mae', 'mse'))
-        choice_optimizer = st.sidebar.selectbox('Optimizer', ('adam', 'sgd'))
-        if st.sidebar.button('Run NNMF Algo'):
-            # history, results, model = run_neural_network(1)
-            # st.write('model: ', model.metrics_names)
-            # st.bar_chart(history.history['loss'])
-            # st.bar_chart(history.history['mae'])
-            st.write("TEST")
+        dataset1 = pd.read_csv("../data/ml-latest-small/ratings.csv", usecols=['userId', 'movieId', 'rating'])
+        dataset1 = dataset1.pivot(index='userId', columns='movieId', values='rating')
 
+        st.write('pivoted data: ', dataset1.head(20))
+
+    # output of data
+
+
+
+
+    # button for knn algo
+    if st.sidebar.button('Run algorithms'):
+        top_n, error = apply_kNN_movie(int(threshold), str(choice_of_algo), False)
+        # get_top_n(result)
+        st.write("Placeholder")
+
+        # loading the history of the nnmf model with mae and loss information
+        history = np.load('../data/model_data/ml/history.npy', allow_pickle='TRUE').item()
+
+        st.write("MAE over 12 epochs")
+        # st.write('model: ', model.metrics_names)
+        df_bar = pd.DataFrame([history['val_mae'][11], error]) # , columns=['NNMF', 'KNN']
+
+        st.bar_chart(df_bar, width=600)
+
+        st.write(top_n[1])
+        new = pd.DataFrame.from_dict(top_n, orient='index')
+        st.write(new.head())
+        print(new.head())
 
 if __name__ == '__main__':
     main()
