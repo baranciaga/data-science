dca750252c6effb7d9d0857a1e8faf5f5c7c4fe5
baranciaga <baranagit@hotmail.de>
2021-06-22 01:24:47 +0200

    added the id to movie title method to utils.py

--- scripts/nnmf_movie_rec.py
+++ scripts/nnmf_movie_rec.py
@@ -1,19 +1,9 @@
-from sklearn.datasets import dump_svmlight_file
 import numpy as np
 import pandas as pd
-import os
-import urllib
-import zipfile
 from sklearn.model_selection import train_test_split
-import shutil
 import matplotlib.pyplot as plt
 import warnings
-from surprise import Reader
-# TF Modules
-import tensorflow as tf
 from tensorflow import keras
-# from keras.optimizers import Adam
-from utils import load_dataset
 warnings.filterwarnings('ignore')
 
 
@@ -31,7 +21,7 @@ def recommend(movie_embedding_learnt, user_embedding_learnt, user_id, number_of_
     return mids
 
 
-def run_neural_network(df):
+def run_neural_network(df, threshold):
     """
     Args: the dataframe to train the model
         df:
@@ -44,6 +34,16 @@ def run_neural_network(df):
     # load the dataset as a pandas dataframe -> get information out of df.
     dataset1 = pd.read_csv("../data/ml-latest-small/ratings.csv", usecols=['userId', 'movieId', 'rating'])
     # dataset1 = load_dataset()
+
+
+    # Tabelle in user-item matrix umwandeln. rows = users, columns = items
+    dataset1 = dataset1.pivot(index = 'userId', columns ='movieId', values = 'rating')
+    # columns rauslöschen, wo Anzahl ratings < thresh
+    dataset1.dropna(thresh=threshold, axis=1, inplace=True)
+    # Matrix wieder in Tabellenform umwandeln. Table: userId, movieId, ratings sortiert nach userId
+    dataset1 = dataset1.stack().reset_index().sort_values(by=['userId', 'movieId'], axis=0)
+    dataset1.columns = ['userId', 'movieId', 'rating']
+
     dataset1.userId = dataset1.userId.astype('category').cat.codes.values
     dataset1.movieId = dataset1.movieId.astype('category').cat.codes.values
 
@@ -53,7 +53,8 @@ def run_neural_network(df):
     n_users, n_movies = len(dataset1.userId.unique()), len(dataset1.movieId.unique())
     print(n_users, '\t', n_movies)
     # number of features. In this case genres, appearing actors etc.
-    n_latent_factors = 5
+    n_latent_factors = 20
+
 
     # The model: First layer is an input layer with
     movie_input = keras.layers.Input(shape=[1], name='Item')
@@ -69,7 +70,6 @@ def run_neural_network(df):
     # dot product
     prod = keras.layers.dot([movie_vec, user_vec], axes=1, name='DotProduct')
     model = keras.Model([user_input, movie_input], prod)
-
     # compiling the model, specifying the optimizer and the loss function, adam is best for
     # recommender systems usually. See https://ruder.io/optimizing-gradient-descent/
     model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae', 'mse'])
@@ -77,8 +77,9 @@ def run_neural_network(df):
 
 
 
+
     # training of the model
-    history = model.fit([train.userId, train.movieId], train.rating, epochs=20, verbose=1)
+    history = model.fit([train.userId, train.movieId], train.rating, epochs=12, verbose=1, validation_data=([test.userId, test.movieId], test.rating))
     results = model.evaluate((test.userId, test.movieId), test.rating, batch_size=32)
 
     movie_embedding_learnt = model.get_layer(name='Movie-Embedding').get_weights()[0]
@@ -88,18 +89,25 @@ def run_neural_network(df):
     print(pd.DataFrame(user_embedding_learnt).describe())
     print(history.history.keys())
     print(history.history['loss'])
-
-    pd.Series(history.history['loss']).plot(logy=True)
+    """
+    pd.Series(history.history['mae']).plot(logy=True)
+    pd.Series(history.history['val_mae']).plot(logy=True) # orange
     plt.xlabel("Epoch")
-    plt.ylabel("Training Error")
+    plt.ylabel("MAE")
     plt.show()
+    """
+    model.save("../data/model_data/ml/my_model.h5")
+    np.save('../data/model_data/ml/history.npy', history.history)
+    np.save('../data/model_data/ml/movie_embedding.npy', movie_embedding_learnt)
+    np.save('../data/model_data/ml/user_embedding.npy', user_embedding_learnt)
+
 
     #
-    recommend(movie_embedding_learnt, user_embedding_learnt, user_id=1)
+    print(recommend(movie_embedding_learnt, user_embedding_learnt, user_id=1))
 
     # print(model.metrics_names)
 
     return history, results, model
 
 
-run_neural_network(df=1)
+run_neural_network(df=1, threshold=100)
